9946d536b01981e9e39e405a1ef4c97c289220eb
Lakentio <124511357+Lakentio@users.noreply.github.com>
2025-03-05 19:57:16 -0300

    Package Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autowallpaper",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     install_requires=[],
     entry_points={
